a0e379ca6ced66aa91252e7169171a0a181a8c1e
jayeshdesai22 <43316609+jayeshdesai22@users.noreply.github.com>
2019-05-03 12:36:25 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
 
 	<form>
 		
-			<h3><u>CONTACT INFORMATION</u></h3>
+			<h3><u>CONTACT</u></h3>
 			
 		 
   				<p> <h2 style="color: #712020"><u>Contact us:</u></h2>  <h4 style="color: #666666">MOBILE NO:612598756</h4> <h4 style="color: #666666">Email:green.video33@gmail.com</h4> </p>
